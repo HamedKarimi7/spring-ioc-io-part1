28b2d1c1b6296617bad042fa31f24b40705e8e30
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-06-13 16:19:10 +0200

    create model + data access package +student class

---
+++ src/main/java/se/lexicon/StudentDao.java
@@ -0,0 +1,9 @@
+package se.lexicon;
+
+
+
+public interface StudentDao {
+
+
+
+}
---
+++ src/main/java/se/lexicon/model/student.java
@@ -0,0 +1,46 @@
+package se.lexicon.model;
+
+
+import java.util.Objects;
+
+public class student {
+
+    private int id;
+    private String name;
+
+    public student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public student(String name) {this.name = name;}
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof student)) return false;
+        student student = (student) o;
+        return getId() == student.getId() && Objects.equals(getName(), student.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
