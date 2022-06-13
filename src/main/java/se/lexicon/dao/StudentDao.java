36825d5a7644395812b4cf814ce19b961276d1f2
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-06-14 00:07:17 +0200

    Exception

--- src/main/java/se/lexicon/dao/StudentDao.java
+++ src/main/java/se/lexicon/dao/StudentDao.java
@@ -1,6 +1,7 @@
 package se.lexicon.dao;
 
 
+import se.lexicon.exception.DataNotFoundException;
 import se.lexicon.model.Student;
 
 import java.util.List;
@@ -9,10 +10,10 @@ public interface StudentDao {
 
     Student save(Student student);
 
-    Student find(int id);
+    Student find(int id) throws DataNotFoundException;
 
     List<Student> findAll();
 
-    void delete(int id);
+    void delete(int id) throws DataNotFoundException;
 
 }
