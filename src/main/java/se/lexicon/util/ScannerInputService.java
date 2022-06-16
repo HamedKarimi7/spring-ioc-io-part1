bce070fb6ecfb79dd6bc31b27b492909961afd9f
Hamed Karimi <hmdkrm.rtj@gmail.com>
2022-06-16 09:00:25 +0200

    define @Bean in AppConfig

--- src/main/java/se/lexicon/util/ScannerInputService.java
+++ src/main/java/se/lexicon/util/ScannerInputService.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.Scanner
 
-@Component
-public class ScannerInputService implements UserInputService {
+     @Component
+     public class ScannerInputService implements UserInputService {
 
-    private Scanner scanner;
+     private Scanner scanner;
 
      @Autowired
-    public ScannerInputService(Scanner scanner) {this.scanner = scanner;}
+     public ScannerInputService(Scanner scanner) {this.scanner = scanner;}
 
-    @Override
-    public String getString() {
+     @Override
+     public String getString() {
         return null;
     }
 
